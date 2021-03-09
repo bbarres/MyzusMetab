48535626a2cf4c895564145943a0d40caa41b067
bbarres <benoit.barres@gmail.com>
2021-03-09 12:36:24 +0100

    setting up the project

---
+++ MyMetab_load.R
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Pour l’analyse, nous avions décidé des critères suivants :
+#   
+#   -          “For each concentration that was tested, three replicates or more, 
+# involving at least 10 L1 per replicate, were tested. Tests were repeated until
+# a minimum of 45 aphids per dose were assayed for each modality.”
+# 
+# -          Mortalité dans le témoin < 20%
+# 
+# -          Pour qu’un test sur un clone donné soit valide, le test sur la 
+# référence 11-0037-0001 correspondant doit être valable.
+# 
+# -          En plus de ces critères stricts, lors des premières analyses R, 
+# nous supprimions parfois les données des doses extrêmes lorsque le plateau 
+# était trop grand, pour que le modèle gère mieux ces résultats. Nous faisions 
+# ça manuellement.
+
