52f2ed1514776b513d38b96abd3fc2095f52fb39
bbarres <benoit.barres@gmail.com>
2021-05-17 15:38:09 +0200

    correction of the name of the sampling ID

--- MyMetab_drc.R
+++ MyMetab_drc.R
@@ -43,8 +43,8 @@ for (i in 1: dim(table(ContCheData$dat_test))[1]) {
 
   }
 
-plot(log(REZcont$LD50))
-plot(log(REZcont$LD50.PBO))
+plot(log(REZcont$LD50),ylab="Death rate")
+plot(log(REZcont$LD50.PBO),ylab="Death rate")
 
 #to remove : 20170503, 20170312 ?, 20180320
 
@@ -89,7 +89,7 @@ for (i in 1: dim(table(dataMyCor$ech_id))[1]) {
   plot(temp.m1,ylim=c(0,1),col=c("black","red"),pch=c(16,17),
        lty=1,main=names(table(dataMyCor$ech_id))[i],
        legend=FALSE,add=TRUE)
-  tempx<-data.frame("ech_id"=names(table(dataMyCor$dat_test))[i],
+  tempx<-data.frame("ech_id"=names(table(dataMyCor$ech_id))[i],
                     "LD50"=temp[1],"SE"=temp[3],
                     "LD50.PBO"=temp[2],"SE.PBO"=temp[4],
                     "EDcomp.est"=temp0[1],
