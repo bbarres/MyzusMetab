b8b30ed964da47613c9b2c917c49b6a593ca2cac
bbarres <benoit.barres@gmail.com>
2022-09-15 18:51:16 +0200

    adding a graph for the prediction of the genotype

--- MyMetab_glm.R
+++ MyMetab_glm.R
@@ -90,7 +90,57 @@ summary(mod1)
 summary(modPBO.1)
 summary(modDif1)
 
+#figure for the mod1
+plot(sumDat$CY3_EXP,log(sumDat$LC50),type="n",las=1)
+points(sumDat[sumDat$nAChR.81=="TT",]$CY3_EXP,
+       log(sumDat[sumDat$nAChR.81=="TT",]$LC50),pch=16,col="red")
+points(sumDat[sumDat$nAChR.81=="RT",]$CY3_EXP,
+       log(sumDat[sumDat$nAChR.81=="RT",]$LC50),pch=16,col="orange")
+points(sumDat[sumDat$nAChR.81=="RR",]$CY3_EXP,
+       log(sumDat[sumDat$nAChR.81=="RR",]$LC50),pch=16,col="green")
 
+genoAch<-factor(rep("TT",251))
+xv<-seq(0,25,0.1)
+yv<-predict(mod1,list(CY3_EXP=xv,nAChR.81=genoAch))
+lines(xv,yv,col="red")
+
+genoAch<-factor(rep("RT",251))
+xv<-seq(0,25,0.1)
+yv<-predict(mod1,list(CY3_EXP=xv,nAChR.81=genoAch))
+lines(xv,yv,col="orange")
+
+genoAch<-factor(rep("RR",251))
+xv<-seq(0,25,0.1)
+yv<-predict(mod1,list(CY3_EXP=xv,nAChR.81=genoAch))
+lines(xv,yv,col="green")
+
+
+#figure for the modPBO.1
+plot(sumDat$CY3_EXP,sumDat$LC50/sumDat$LC50.PBO,type="n",las=1)
+points(sumDat[sumDat$nAChR.81=="TT",]$CY3_EXP,
+       sumDat[sumDat$nAChR.81=="TT",]$LC50/sumDat[sumDat$nAChR.81=="TT",]$LC50.PBO,
+       pch=16,col="red")
+points(sumDat[sumDat$nAChR.81=="RT",]$CY3_EXP,
+       sumDat[sumDat$nAChR.81=="RT",]$LC50/sumDat[sumDat$nAChR.81=="RT",]$LC50.PBO,
+       pch=16,col="orange")
+points(sumDat[sumDat$nAChR.81=="RR",]$CY3_EXP,
+       sumDat[sumDat$nAChR.81=="RR",]$LC50/sumDat[sumDat$nAChR.81=="RR",]$LC50.PBO,
+       pch=16,col="green")
+
+genoAch<-factor(rep("TT",251))
+xv<-seq(0,25,0.1)
+yv<-predict(modPBO.1,list(CY3_EXP=xv,nAChR.81=genoAch))
+lines(xv,yv,col="red")
+
+genoAch<-factor(rep("RT",251))
+xv<-seq(0,25,0.1)
+yv<-predict(modPBO.1,list(CY3_EXP=xv,nAChR.81=genoAch))
+lines(xv,yv,col="orange")
+
+genoAch<-factor(rep("RR",251))
+xv<-seq(0,25,0.1)
+yv<-predict(modPBO.1,list(CY3_EXP=xv,nAChR.81=genoAch))
+lines(xv,yv,col="green")
 
 
 ##############################################################################/
