9a7acd4122cbaa0884dae8d4a8adff6726ce3583
bbarres <benoit.barres@gmail.com>
2023-10-19 18:03:04 +0200

    adding model for link between expression level and phenotype for [RR] genotype

---
+++ MyMetab_metabR_nls.R
@@ -0,0 +1,75 @@
+##############################################################################/
+##############################################################################/
+#Regression of CYP6CY3 expression against metabolic resistant proportion
+##############################################################################/
+##############################################################################/
+
+#loading the packages necessary for the analysis
+source("MyMetab_load.R")
+#limiting the dataset to the R81T-[RR] genotypes
+sumDatRR<-sumDat[sumDat$nAChR.81=="RR",]
+#adding a column for the proportion of LD50 linked to metabolic resistance
+sumDatRR$propMeta<-(1-(temp$LC50.PBO/temp$LC50))
+sumDatRR<-sumDatRR[order(sumDatRR$CY3_EXP),]
+
+
+
+##############################################################################/
+#Fitting a Weibull model####
+##############################################################################/
+
+Weib.m1<-drm(sumDatRR$propMeta~temp$CY3_EXP,
+             data=sumDatRR,fct=W1.2())
+plot(Weib.m1,type="confidence",log="",col="blue",lwd=3,lty=2)
+plot(Weib.m1,type="obs",add=TRUE)
+summary(Weib.m1)
+
+Micha.m1<-drm(sumDatRR$propMeta~temp$CY3_EXP,
+             data=sumDatRR,fct=MM.3())
+plot(Micha.m1)
+summary(Micha.m1)
+
+ExpoDec.m1<-drm(sumDatRR$propMeta~temp$CY3_EXP,
+                data=sumDatRR,fct=EXD.3())
+plot(ExpoDec.m1)
+summary(ExpoDec.m1)
+
+AssyReg.m1<-drm(sumDatRR$propMeta~temp$CY3_EXP,
+              data=sumDatRR,fct=AR.2())
+plot(AssyReg.m1,type="confidence",log="",col="skyblue3",lwd=3,lty=2,
+     xlab="CYP6CY3 expression level",
+     ylab="Proportion of metabolic resistance",
+     ylim=c(0,1))
+plot(AssyReg.m1,type="obs",pch=19,col="green3",add=TRUE)
+summary(AssyReg.m1)
+
+
+
+##############################################################################/
+#Negative exponential model fitting####
+##############################################################################/
+
+# Prepare a good inital state
+theta.0 <- max(sumDatRR$propMeta) * 1.1
+model.0 <- lm(log(-propMeta+theta.0)~CY3_EXP,data=sumDatRR)
+alpha.0 <- -exp(coef(model.0)[1])
+beta.0 <- coef(model.0)[2]
+
+debut<-list(alpha=alpha.0,beta=beta.0,theta=theta.0)
+
+# Fit the model
+negExpo.m1<-nls(propMeta~alpha*exp(beta*CY3_EXP)+theta,
+                data=sumDatRR,start=debut)
+
+# add fitted curve
+plot(sumDatRR$CY3_EXP,sumDatRR$propMeta)
+lines(seq(0.01,20.5,by=0.05),
+      predict(negExpo.m1,list(CY3_EXP=seq(0.01,20.5,by=0.05)),
+              interval="confidence",level=0.95),
+      col='darkgreen',lwd=3)
+summary(negExpo.m1)
+
+
+##############################################################################/
+#END
+##############################################################################/
