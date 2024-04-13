1ab4e8576a83748524a8cf4300f9dc03fe4d603c
bbarres <benoit.barres@gmail.com>
2024-04-14 00:43:34 +0200

    more cleaning of the scripts

--- MyMetab_drc.R
+++ MyMetab_drc.R
@@ -46,10 +46,6 @@ for (i in 1: dim(table(ContCheData$dat_test))[1]) {
 plot(log(REZcont$LD50),ylab="Death rate")
 plot(log(REZcont$LD50.PBO),ylab="Death rate")
 
-#to remove : 20170503, 20170312 ?, 20180320
-dataMyCor<-dataMyMeta[!dataMyMeta$dat_test %in% 
-                        c("20170503","20170312","20180320"),]
-
 
 ##############################################################################/
 #comparing the LD50 with or without PBO####
@@ -67,7 +63,6 @@ dataMyCor$idd<-paste(dataMyCor$ech_id,dataMyCor$dat_test,
                      sep=".")
 repetorem<-dataMyCor[dataMyCor$mortrate>0.2 & dataMyCor$dose==0,]
 dataMyCor<-dataMyCor[!dataMyCor$idd %in% repetorem$idd,]
-
 
 REZ<-data.frame("ech_id"=as.character(),
                 "LD50"=as.character(),
@@ -128,98 +123,6 @@ dev.off()
 
 #export the result table
 write.table(REZ, file="output/results_bioassay.txt",
-            sep="\t",quote=FALSE,row.names=FALSE)
-
-
-##############################################################################/
-#END
-##############################################################################/
-
-#to trash after confirmation by claire 8 april 2022
-
-nf<-layout(matrix(c(1,1,1,1,2,3),3,2,byrow=TRUE))
-
-datatemp<-dataMyCor[dataMyCor$ana_id %in% c("69","70"),]
-temp.m1<-drm(nb_mtot/(nb_mtot+nb_vi)~dose,
-             data=datatemp,curveid=synerg_id,
-             fct=LN.3u())
-temp0<-EDcomp(temp.m1,c(0.5,0.5),type="absolute")
-temp<-ED(temp.m1,0.5,type="absolute")
-plot(temp.m1,ylim=c(0,1),col=c("black","red"),type="all",
-     lty=1,main="rep1",
-     legendPos=c(50,1),ylab="Death rate")
-plot(temp.m1,ylim=c(0,1),col=c("black","red"),pch=c(16,17),
-     lty=1,main="rep1",
-     legend=FALSE,add=TRUE)
-dattempWout<-datatemp[datatemp$synerg_id!="PBO",]
-temp1<-dattempWout[,c("ech_id","synerg_id","rep_test","test_echec",
-                      "dose","effectif")]
-temp2<-pivot_wider(temp1,names_from="dose",values_from="effectif",
-                   values_fn=mean)
-barplot(as.matrix(temp2[,c(5:dim(temp2)[2])]),las=2)
-dattempWith<-datatemp[datatemp$synerg_id=="PBO",]
-temp3<-dattempWith[,c("ech_id","synerg_id","rep_test","test_echec",
-                      "dose","effectif")]
-temp4<-pivot_wider(temp3,names_from="dose",values_from="effectif",
-                   values_fn=mean)
-temp4[is.na(temp4)]<-0
-barplot(as.matrix(temp4[,c(5:dim(temp4)[2])]),
-        col=brewer.pal(9,"Reds")[c(7,5,3)],las=2)
-tempx<-data.frame("ech_id"="rep1",
-                  "LD50"=temp[1],"SE"=temp[3],
-                  "mean/dose"=mean(colSums(
-                    as.matrix(temp2[,c(5:dim(temp2)[2])])),
-                    na.rm=TRUE),
-                  "LD50.PBO"=temp[2],"SE.PBO"=temp[4],
-                  "mean/dose.PBO"=mean(colSums(
-                    as.matrix(temp4[,c(5:dim(temp4)[2])])),
-                    na.rm=TRUE),
-                  "EDcomp.est"=temp0[1],
-                  "EDcomp.pval"=temp0[4])
-REZ<-rbind(REZ,tempx)
-
-
-datatemp<-dataMyCor[dataMyCor$ana_id %in% c("1855","1856"),]
-temp.m1<-drm(nb_mtot/(nb_mtot+nb_vi)~dose,
-             data=datatemp,curveid=synerg_id,
-             fct=LN.3u())
-temp0<-EDcomp(temp.m1,c(0.5,0.5),type="absolute")
-temp<-ED(temp.m1,0.5,type="absolute")
-plot(temp.m1,ylim=c(0,1),col=c("black","red"),type="all",
-     lty=1,main="rep2",
-     legendPos=c(50,1),ylab="Death rate")
-plot(temp.m1,ylim=c(0,1),col=c("black","red"),pch=c(16,17),
-     lty=1,main="rep2",
-     legend=FALSE,add=TRUE)
-dattempWout<-datatemp[datatemp$synerg_id!="PBO",]
-temp1<-dattempWout[,c("ech_id","synerg_id","rep_test","test_echec",
-                      "dose","effectif")]
-temp2<-pivot_wider(temp1,names_from="dose",values_from="effectif",
-                   values_fn=mean)
-barplot(as.matrix(temp2[,c(5:dim(temp2)[2])]),las=2)
-dattempWith<-datatemp[datatemp$synerg_id=="PBO",]
-temp3<-dattempWith[,c("ech_id","synerg_id","rep_test","test_echec",
-                      "dose","effectif")]
-temp4<-pivot_wider(temp3,names_from="dose",values_from="effectif",
-                   values_fn=mean)
-temp4[is.na(temp4)]<-0
-barplot(as.matrix(temp4[,c(5:dim(temp4)[2])]),
-        col=brewer.pal(9,"Reds")[c(7,5,3)],las=2)
-tempx<-data.frame("ech_id"="rep2",
-                  "LD50"=temp[1],"SE"=temp[3],
-                  "mean/dose"=mean(colSums(
-                    as.matrix(temp2[,c(5:dim(temp2)[2])])),
-                    na.rm=TRUE),
-                  "LD50.PBO"=temp[2],"SE.PBO"=temp[4],
-                  "mean/dose.PBO"=mean(colSums(
-                    as.matrix(temp4[,c(5:dim(temp4)[2])])),
-                    na.rm=TRUE),
-                  "EDcomp.est"=temp0[1],
-                  "EDcomp.pval"=temp0[4])
-REZ<-rbind(REZ,tempx)
-
-#export the result table
-write.table(REZ, file="output/results_13-0284-0034.txt",
             sep="\t",quote=FALSE,row.names=FALSE)
 
 
